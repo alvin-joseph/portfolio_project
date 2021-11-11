5c9460d2e7c2b52787ae977410f0255d9b29e625
alvin-joseph <aznman1234@gmail.com>
2021-11-11 01:53:48 -0600

    updated project section/styling

--- src/components/Cards/CardItems.jsx
+++ src/components/Cards/CardItems.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function CardItems(props) {
     return (
-        <div>
+        <div className="card_item">
             <li className="cards__item">
                 <div className="cards__item__link">
                     <a href={props.path} target="_blank" rel="noreferrer">
@@ -11,8 +11,6 @@ function CardItems(props) {
                         </figure>
                     </a>
                     <div className="cards__item__info">
-                        <h5 className="cards__item__text">{props.text}</h5>
-                        <h4 className="cards__item__desc">{props.description}</h4>
                         <a href={props.github} target="_blank" rel="noreferrer" className="cards__item__git" >
                             <img src="images/github.png" alt="GitHub" />
                         </a>
